dbd90c30de386123cd4f17eee975223aa501a441
Kinga <zawarczynska.ko@gmail.com>
2020-01-16 23:40:11 +0100

    added some stylings to pagination

--- hooks/src/components/Pagination.js
+++ hooks/src/components/Pagination.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import styles from './Pagination.modules.css'
 
 export const Pagination = ({postsPerPage, totalPosts, paginate}) => {
     const pageNumbers = [];
@@ -9,17 +10,17 @@ export const Pagination = ({postsPerPage, totalPosts, paginate}) => {
 
     return (
         <nav>
-            <ul className="pagination">
+            <div className="pagination">
                 {pageNumbers.map(number => (
-                    <li key={number} className="page-item">
+                    <div key={number} className="page-item">
                         <a onClick={() => {paginate(number)
                         }} href="!#" className="page-link">
                             {number}
                             </a>                        
 
-                    </li>
+                    </div>
                 ))}
-            </ul>
+            </div>
 
             
         </nav>
