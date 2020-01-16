067783928248ee3d6faa75adf838174adc548218
Kinga <zawarczynska.ko@gmail.com>
2020-01-16 23:30:55 +0100

    added pagination and its logic

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import './App.css';
 import { Posts } from './components/Posts';
+import { Pagination } from './components/Pagination';
 
 const App = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage]= useState(10);
+  const [postsPerPage]= useState(10);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -25,13 +26,16 @@ const App = () => {
   const indexOfFirtsPost = indexOfLastPost-postsPerPage;
   const currentPosts = posts.slice(indexOfFirtsPost, indexOfLastPost)
 
-
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  }
   return (
     <div className="App">
       <h1 className="text-primary">
         My Blog
       </h1>
       <Posts posts={currentPosts} loading={loading} />
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate}/>
     
     </div>
   );
